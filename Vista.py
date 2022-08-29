b98540ef8e745796ddd7e57710a2a07a004471b7
davidzamora9aSyC <79547839+davidzamora9aSyC@users.noreply.github.com>
2022-08-29 15:29:24 -0500

    Update Vista.py

--- Vista.py
+++ Vista.py
@@ -4,9 +4,11 @@ f = c.abrirArchivo("C:\\Users\\Administrator\\Documents\\Universidad\\4\\lym\\1\
 if c.initProg(f): #verifica que se declare un nuevo programa correctamente. Devuelve boolean
     res, variables = c.initVar(f)     #verifica que se declaren variables correctamente. Devuelve  (boolean, {variable1:'', variable2:''...})
     if res:
-        PROCs = c.Procedures(f) #Retorna una lista de listas. La lista grande contiene listas con las lineas de las definiciones de procedimientos
+        PROCs, diccionarioParametros = c.Procedures(f) 
+                                #si no esta bien hecha la declaracion retorna (False, False)
+                                #Retorna una tupla:
+                                #el primer elemento es unalista de listas. La lista grande contiene listas con las lineas de las definiciones de procedimientos
                                 #EJ: [['PROC putCB (c, b)', '{', 'drop (c);', 'free (b);', 'walk (n)', '}', 'CORP'], ['PROC ', '{', 'while ( canWalk (north ,1) ) do { walk (north ,1) } od', '}', 'CORP']]
-                                #Si la definicion de un procedimiento esta mal hecha retorna False
-        print(PROCs)
+                                #el segundo elemento es un diccionario de la forma {nombreprocedimiento1: ['parametro1','parametro2']}
         
         
